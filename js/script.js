bc0784e3d8a082e33eaf18dbbb931451230f68df
RKT Void <141272415+stylro@users.noreply.github.com>
2023-10-08 20:11:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*var crsr = document.querySelector("#crusor");
+
+document.addEventListener("mousemove", function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+
+    crsr.style.left = x + "px";
+    crsr.style.top = y + "px";
+
+});*/
